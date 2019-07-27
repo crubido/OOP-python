f710795280f6c488241453842ff809f636fc0881
Carlos Rubido <carlosrubido@Carloss-MBP-2.attlocal.net>
2019-07-27 12:37:27 -0400

    Added methods: cartesian_form, polar_form and __str__

--- point.py
+++ point.py
@@ -1,5 +1,19 @@
+import math
+
+
 class Point:
 
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
+        self.radius = math.sqrt(self.x * self.x + self.y * self.y)
+        self.angle = math.atan2(self.x, self.y)
+
+    def cartesian_form(self):
+        return (self.x, self.y)
+
+    def polar_form(self):
+        return (self.radius, self.angle)
+
+    def __str__(self):
+        return "(" + str(self.x) + ", " + str(self.y) + ")"
